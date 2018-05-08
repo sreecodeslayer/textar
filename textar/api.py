9c43589fc41c241425951f2bc612de9a695fdc17
Sreenadh TC <kesav.tc8@gmail.com>
2018-05-08 11:45:34 +0530

    Update archiving api to handle invalid / non existant directories or files

--- textar/api.py
+++ textar/api.py
@@ -22,28 +22,33 @@ def make_archive(args):
     print('Archiving into %s ...' % out_file)
 
     boundary = uuid4().hex
-    with open(out_file, 'w') as out:
-        out.write('boundary: %s\n\n' % boundary)
-        for input_file in input_files:
-            # Start of a file, add boundary line
-            filename = ntpath.basename(input_file)
+    try:
+        with open(out_file, 'w') as out:
+            out.write('boundary: %s\n\n' % boundary)
+            for input_file in input_files:
+                # Start of a file, add boundary line
+                filename = ntpath.basename(input_file)
 
-            boundary_line = '%s %s\n' % (boundary, filename)
-            out.write(boundary_line)
+                boundary_line = '%s %s\n' % (boundary, filename)
+                out.write(boundary_line)
 
-            # Copy contents from input file
-            print('Add file `%s` to archive' % filename)
-            try:
-                with open(input_file) as inp:
-                    out.writelines(inp.readlines())
-            except FileNotFoundError as e:
-                print(e)
-                os.remove(out_file)
-                print('Exiting ...')
-                sys.exit(0)
-        else:
-            return False
-    return True
+                # Copy contents from input file
+                print('Add file `%s` to archive' % filename)
+                try:
+                    with open(input_file) as inp:
+                        out.writelines(inp.readlines())
+                except FileNotFoundError as e:
+                    print(e)
+                    os.remove(out_file)
+                    print('Exiting ...')
+                    sys.exit(0)
+            else:
+                return False
+        return True
+    except FileNotFoundError as e:
+        print(e)
+        print('Exiting ...')
+        sys.exit(0)
 
 
 def validate_txr(filepath):
@@ -94,7 +99,7 @@ def extract(txr_file):
     for line in file_content:
         if line.startswith(boundary):
             filename = line.split(' ', 1)[-1].strip()
-            new_file = os.path.join(cwd, '_'+filename)
+            new_file = os.path.join(cwd, filename)
             print(filename)
         elif new_file:
             with open(new_file, 'a') as out:
